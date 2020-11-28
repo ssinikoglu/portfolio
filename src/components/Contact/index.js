9f9b901e12ae501afb70ec38d2bca633edfe16f2
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-28 22:03:41 +0300

    Feat: validate contact form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,64 +1,106 @@
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
 import React from "react";
-import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from "mdbreact";
-
+import {
+  MDBMask,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBBtn,
+  MDBIcon,
+} from "mdbreact";
+import { ReactComponent as ContactImage } from "../../images/contact.svg";
 const Contact = () => {
   return (
-    <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form>
-            <p className="h4 text-center mb-4">Keep in touch</p>
-            <label htmlFor="defaultFormContactNameEx" className="grey-text">
-              Name
-            </label>
-            <input
-              type="text"
-              id="defaultFormContactNameEx"
-              className="form-control"
-            />
-            <br />
-            <label htmlFor="defaultFormContactEmailEx" className="grey-text">
-              E-mail
-            </label>
-            <input
-              type="email"
-              id="defaultFormContactEmailEx"
-              className="form-control"
-            />
-            <br />
-            <label htmlFor="defaultFormContactSubjectEx" className="grey-text">
-              Subject
-            </label>
-            <input
-              type="text"
-              id="defaultFormContactSubjectEx"
-              className="form-control"
-            />
-            <br />
-            <label htmlFor="defaultFormContactMessageEx" className="grey-text">
-              Message
-            </label>
-            <textarea
-              type="text"
-              id="defaultFormContactMessageEx"
-              className="form-control"
-              rows="3"
-            />
-            <div className="text-center mt-4">
-              <MDBBtn color="warning" outline type="submit">
-                Send
-                <MDBIcon far icon="paper-plane" className="ml-2" />
-              </MDBBtn>
-            </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+    <>
+      <MDBContainer>
+        <MDBRow>
+          <MDBMask
+            overlay="black"
+            className="flex-center flex-row black-text text-left"
+          >
+            <MDBCol>
+              <MDBCol md="8" className="ml-5">
+                <form className="needs-validation" novalidate>
+                  <p className="h4 text-center mb-4">Keep in touch</p>
+                  <label
+                    htmlFor="validationCustom01"
+                    className="black-text font-weight-bold"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="validationCustom01"
+                    className="form-control"
+                    required
+                  />
+                  <br />
+                  <label
+                    htmlFor="defaultFormContactEmailEx"
+                    className="black-text font-weight-bold"
+                  >
+                    E-mail
+                  </label>
+                  <input
+                    type="email"
+                    id="defaultFormContactEmailEx"
+                    className="form-control"
+                    required
+                  />
+                  <br />
+                  <label
+                    htmlFor="defaultFormContactSubjectEx"
+                    className="black-text font-weight-bold"
+                  >
+                    Subject
+                  </label>
+                  <input
+                    type="text"
+                    id="defaultFormContactSubjectEx"
+                    className="form-control font-weight-bold"
+                  />
+                  <br />
+                  <label
+                    htmlFor="defaultFormContactMessageEx"
+                    className="black-text font-weight-bold"
+                    required
+                  >
+                    Message
+                  </label>
+                  <textarea
+                    type="text"
+                    id="defaultFormContactMessageEx"
+                    className="form-control"
+                    rows="3"
+                  />
+                  <div className="text-left mt-4">
+                    <MDBBtn
+                      color="black font-weight-bold"
+                      outline
+                      type="submit"
+                      className="waves-effect"
+                      aria-hidden="true"
+                    >
+                      Send
+                      <MDBIcon far icon="paper-plane" className="ml-2" />
+                    </MDBBtn>
+                  </div>
+                </form>
+              </MDBCol>
+            </MDBCol>
+            <MDBCol>
+              <ContactImage
+                className="img-fluid"
+                style={{
+                  float: "right",
+                  width: "500px",
+                }}
+              />
+            </MDBCol>
+          </MDBMask>
+        </MDBRow>
+      </MDBContainer>
+    </>
   );
 };
 
 export default Contact;
-

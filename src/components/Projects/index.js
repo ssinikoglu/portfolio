a98dc647adb663cc9a3616e7346634ae86a1fb4a
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-28 22:12:10 +0300

    Refactor: delete unused imports

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,4 @@
 import React from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
 
 const Projects=()=>{
     return(

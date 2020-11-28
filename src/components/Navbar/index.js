bc7b03f4c71179e1b7fca0bfbf0f80a7cb4249a5
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-28 22:09:10 +0300

    Perf: change navbar collapsing setting

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,100 +1,94 @@
-import React from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "bootstrap-css-only/css/bootstrap.min.css";
-import "mdbreact/dist/css/mdb.css";
+import React, { Component } from "react";
 import {
   MDBNavbar,
-  MDBNavbarBrand,
   MDBNavbarNav,
-  MDBNavbarToggler,
   MDBCollapse,
   MDBNavItem,
   MDBNavLink,
-  MDBContainer,
+  MDBNavbarToggler,
   MDBIcon,
-  MDBMask,
-  MDBView,
 } from "mdbreact";
-import { BrowserRouter as Router } from "react-router-dom";
+class Navbar extends Component {
+  state = {
+    isOpen: false,
+  };
 
-class Navbar extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      collapse: false,
-      isWideEnough: false,
-    };
-    this.onClick = this.onClick.bind(this);
-  }
+  toggleCollapse = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  };
 
-  onClick() {
-    this.setState({
-      collapse: !this.state.collapse,
-    });
-  }
   render() {
     return (
-      <div>
-        <header>
-          <Router>
-            <MDBNavbar
-              color="transparent"
-              fixed="top"
-              dark
-              expand="md"
-              scrolling
-              transparent
-            >
-              <MDBCollapse isOpen={this.state.collapse} navbar>
-                <MDBNavbarNav right>
-                  <MDBNavItem>
-                    <MDBNavLink to="/">
-                      <strong className="black-text">Home</strong>
-                    </MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="/about">
-                      <b className="black-text">About</b>
-                    </MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="/projects">
-                      <b className="black-text">Projects</b>
-                    </MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="/contact">
-                      <b className="black-text">Contact</b>
-                    </MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="https://github.com/ssinikoglu">
-                      <MDBIcon
-                        fab
-                        icon="github"
-                        size="1x"
-                        className="black-text pr-3"
-                      />
-                    </MDBNavLink>
-                  </MDBNavItem>
-                  <MDBNavItem>
-                    <MDBNavLink to="https://www.linkedin.com/in/sulesinikoglu/">
-                      <MDBIcon
-                        fab
-                        icon="linkedin"
-                        size="1x"
-                        className="black-text pr-3"
-                      />
-                    </MDBNavLink>
-                  </MDBNavItem>
-                </MDBNavbarNav>
-              </MDBCollapse>
-            </MDBNavbar>
-          </Router>
-        </header>
-      </div>
+      <MDBNavbar
+        color="transparent"
+        fixed="top"
+        dark
+        expand="md"
+        scrolling
+        transparent
+      >
+        <MDBNavbarToggler onClick={this.toggleCollapse} className="black" />
+        <MDBCollapse id="navbarCollapse3" isOpen={this.state.isOpen} navbar>
+          <MDBNavbarNav right>
+            <MDBNavItem active>
+              <MDBNavLink to="/">
+                <strong
+                  className="black-text pr-4 waves-effect"
+                  aria-hidden="true"
+                >
+                  Home
+                </strong>
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/about">
+                <b className="black-text pr-4 waves-effect">About</b>
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/projects">
+                <b className="black-text pr-4 waves-effect">Projects</b>
+              </MDBNavLink>
+            </MDBNavItem>
+            <MDBNavItem>
+              <MDBNavLink to="/contact">
+                <b className="black-text pr-4 waves-effect">Contact</b>
+              </MDBNavLink>
+            </MDBNavItem>
+          </MDBNavbarNav>
+          <MDBNavbarNav right className="social-media-icons waves-effect">
+            <MDBNavItem>
+              <a
+                href="https://github.com/ssinikoglu"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <MDBIcon
+                  fab
+                  icon="github"
+                  size="2x"
+                  className="black-text pr-4"
+                />
+              </a>
+            </MDBNavItem>
+            <MDBNavItem>
+              <a
+                href="https://www.linkedin.com/in/sulesinikoglu/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <MDBIcon
+                  fab
+                  icon="linkedin"
+                  size="2x"
+                  className="black-text pr-4"
+                />
+              </a>
+            </MDBNavItem>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBNavbar>
     );
   }
 }
-
 export default Navbar;

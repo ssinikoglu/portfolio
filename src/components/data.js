049685e711efacb3066ca0fbc10af66c8ab4b2bf
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-30 01:24:53 +0300

    add projects data

--- src/components/data.js
+++ src/components/data.js
@@ -1,29 +1,36 @@
- const projects = [
-   {
-     name: "Travel Bucket",
-     desc:
-       "Travel Bucket is a board web site that you can create and list your travel wishes.",
-     tech: ["Firebase", "Material Design Bootstrap", "React"],
-     github: "https://github.com/cuygur/travel-bucket",
-     picture: "Travel_Bucket",
-   },
-   {
-     name: "Mad-Libs",
-     desc: "Mad-Libs game with camping themed story.",
-     tech: ["JavaScript", "HTML", "CSS"],
-     github:
-       "https://github.com/ssinikoglu/bootcamp-istanbul-jul2020/tree/master/madlibz/Sule-Zeynep",
-     picture: "MadLibz",
-   },
-   {
-     name: "Back to the Movies",
-     desc:
-       "Back to the Movies is a movie website similar to IMDB which is built via TMDB API",
-     tech: ["JavaScript", "OOP", "HTML", "CSS"],
-     github:
-       "https://github.com/ibrahimaljarad/bootcamp-istanbul-jul2020/tree/master/movieproject/ibrahim-sule",
-     picture: "Back_to_the_Movies",
-   },
- ];
+import juniper from "../images/juniper.png";
+import backtothemovies from "../images/backtothemovies.png";
+import portfolio from "../images/portfolio.png";
+
+const projects = [
+  {
+    name: "Juniper",
+    desc:
+      "Juniper is created for informing children in the MENA region about environmental issues and climate change with games and resources.",
+    tech: "React, Ant Design, Firebase, i18next, Jest, Figma",
+    github: "https://github.com/ssinikoglu/capstone-istanbul-juniper",
+    picture: juniper,
+    demo: "https://juniper-ist.netlify.app/",
+  },
+  {
+    name: "Back to the Movies",
+    desc:
+      "Back to the Movies offers movie recommendations to cinema lovers via searching for the latest movies, future movies as well as movie genre, popularity.",
+    tech: "JavaScript, OOP, HTML, CSS",
+    github:
+      "https://github.com/ibrahimaljarad/bootcamp-istanbul-jul2020/tree/master/movieproject/ibrahim-sule",
+    picture: backtothemovies,
+    demo: "#",
+  },
+  {
+    name: "Portfolio",
+    desc: "Portfolio is consists of my work experience and projects.",
+    tech: "React, React-Router, Material Design Bootstrap",
+    github:
+      "https://github.com/ssinikoglu/bootcamp-istanbul-jul2020/tree/master/madlibz/Sule-Zeynep",
+    picture: portfolio,
+    demo: "https://sule-sinikoglu.netlify.app/"
+  },
+];
 
 export default projects;

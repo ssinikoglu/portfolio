20ae493ec486daff0de0dc2ef05d78e60b1ae729
Sule Sinikoglu <ssinikoglu@gmail.com>
2020-11-30 20:50:38 +0300

    Style: update about me text styling

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 import { MDBIcon, MDBMask, MDBRow, MDBCol, MDBContainer } from "mdbreact";
 import { ReactComponent as AboutMeImage } from "../../images/about.svg";
 
-
 const AboutMe = () => {
   return (
     <>
@@ -17,22 +16,27 @@ const AboutMe = () => {
           >
             <MDBCol>
               <MDBCol className="ml-5">
-                <div className="mb-3 text-center">
+                <div className="text-center aboutMeTitle">
                   <h2>
                     <strong>Hi there, I’m Sule Sinikoglu</strong>
                   </h2>
                 </div>
-                <p className="text-md-center">
-                  I'm a Front end Developer and curious about how I can develop
-                  well-designed, and high-quality websites and applications. My
-                  vision is to make a positive impact on my code and also share
-                  my experience and support the ones who want to be developers
-                  as much as I can. My educational background differs from my
-                  current role since I want to create a career that consists of
-                  design and tech together. That's why starting <b>Re: Coded</b>{" "}
-                  has become a milestone for me. I'm continuing on this long
-                  journey by taking sustainable steps.
-                </p>
+                <div className=" m-4 text-md-center aboutMeText">
+                  <p className="mb-3">
+                    I'm a Front end Developer and curious about how I can
+                    develop well-designed, and high-quality websites and
+                    applications. My vision is to make a positive impact on my
+                    code and also share my experience and support the ones who
+                    want to be developers as much as I can.{" "}
+                  </p>
+                  <p>
+                    My educational background differs from my current role since
+                    I want to create a career that consists of design and tech
+                    together. That's why starting <b>Re: Coded </b>
+                    has become a milestone for me. I'm continuing on this long
+                    journey by taking sustainable steps.
+                  </p>
+                </div>
                 <div className="text-center mt-5">
                   <a
                     href="https://drive.google.com/file/d/1n7462cZbB-QYXdC9Nt2M6_Qj9STs9uMP/view?usp=sharing"

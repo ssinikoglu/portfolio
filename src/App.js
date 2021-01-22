7ae8d175eb8d6680933196ac50b078606bb94fd3
Sule Sinikoglu <ssinikoglu@gmail.com>
2021-01-23 02:02:56 +0300

    Feat: configure analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router,Route} from "react-router-dom";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomePage from "./containers/HomePage";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
@@ -8,8 +8,13 @@ import Navbar from "./components/Navbar";
 import { MDBView } from "mdbreact";
 import backgroundImage from "./images/backgroundImage.jpeg";
 import "./App.css";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-5Y2M5K0CRM");
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
   return (
     <Router className="App">
       <Route path="/" component={Navbar} />
